faf062ff1a99f573cb84fd875144ab56bca5febf
sahenry sempindu <sahenry@tpbbank.co.tz>
2019-10-18 12:53:59 +0300

    few change

--- application/views/admin/Data_post.php
+++ application/views/admin/Data_post.php
@@ -1,37 +1,40 @@
 <div class="container">
-        
-	<h3><?= $heading; ?></h3>
-	<hr>
-	<a href="<?php echo base_url('pagecontroller/addpost'); ?>" class="btn btn-primary">Add post</a>
-	<br>
-        <table class="table table-bordered table-responsive">
-        	<thead>
-        		<tr>
-        			<th>ID</th>
-        			<th>Tittle</th>
-        			<th>Description</th>
-        			<th>By</th>
-        			<th>posted at</th>
-        			<th>Action</th>
-        		</tr>
-        	</thead>
-        	<tbody>
-        	<?php if($allposts) : ?>
-        		<?php foreach($allposts as $allpost) : ?>
-	        		<tr>
-	        			<td><?php echo $this->session->set_flashdata('success_msg'); ?></td>
-	        			<td><?php echo $allpost->tittle; ?></td>
-	        			<td><?php echo $allpost->description; ?></td>
-	        			<td><?php echo $allpost->username; ?></td>
-	        			<td><?php echo $allpost->cr_at; ?></td>
-	        			<td>
-	        				<a href="<?php echo base_url('pagecontroller/addpost'); ?>" class="btn btn-info">Edit</a>
-	        				<a href="<?php echo base_url(); ?>" class="btn btn-danger">Delete</a>
-	        			</td>
-	        		</tr>
-        		<?php endforeach; ?>
-        	<?php endif; ?>
-        	</tbody>
-                
-        </table>
+        <div class="row">
+                <div class="col-md-12">
+                     <h3><?= $heading; ?></h3> 
+                     <hr>
+                     <a href="<?php echo base_url('pagecontroller/addpost'); ?>" class="btn btn-primary">Add post</a>
+                     <br>
+                     <table class="table table-bordered table-responsive">
+                        <thead>
+                        <tr>
+                                <th>ID</th>
+                                <th>Tittle</th>
+                                <th>Description</th>
+                                <th>By</th>
+                                <th>posted at</th>
+                                <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php if($allposts) : ?>
+                                        <?php foreach($allposts as $allpost) : ?>
+                                                <tr>
+                                                        <td><?php echo $this->session->set_flashdata('success_msg'); ?></td>
+                                                        <td><?php echo $allpost->tittle; ?></td>
+                                                        <td><?php echo $allpost->description; ?></td>
+                                                        <td><?php echo $allpost->username; ?></td>
+                                                        <td><?php echo $allpost->cr_at; ?></td>
+                                                        <td>
+                                                                <a href="<?php echo base_url('pagecontroller/addpost'); ?>" class="btn btn-info">Edit</a>
+                                                                <a href="<?php echo base_url(); ?>" class="btn btn-danger">Delete</a>
+                                                        </td>
+                                                </tr>
+                                        <?php endforeach; ?>
+                                <?php endif; ?>
+                                </tbody>
+                                
+                        </table>   
+                </div>
+        </div>
 </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,21 +2,14 @@
 <html>
 <head>
 	<title><?= $tittle; ?></title>
-	<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap_3_3_7/css/bootstrap.min.css'); ?>">
-	<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap_3_3_7/css/bootstrap-theme.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/bootstrap-3.4.1/css/bootstrap.min.css'; ?>">
 </head>
 <body>
-    <nav class="navbar navbar-dark bg-dark">
+    <div class="navbar bg-dark">
     	<div class="container">
-    		<h2><span class="glyphicon glyphicon-home"></span>&nbsp; Welcome to my blog</h2>
+    		<a href="#" class="navbar navbar-brand">CRUDE APPLICATION</a>
+    		<!-- <h2><span class="glyphicon glyphicon-home"></span>&nbsp; Welcome to my blog</h2> -->
     	</div>
-    </nav>
-
-    <?php if ($this->session->set_flashdata('success_msg')) { ?>
-                <div class="alert alert-success">
-                        <?php echo $this->session->set_flashdata('success_msg'); ?>
-                </div>
-        <?php } ?>
-        
+    </div>
 
 	
